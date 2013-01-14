8ea0779a04b6b99feb1d592e03a9cadc9f18b303
Mathías Montossi <mmontossi@mattways.com>
2013-01-14 12:05:47 -0200

    Bundle update

--- test/rails_slugs_test.rb
+++ test/rails_slugs_test.rb
@@ -2,36 +2,8 @@ require 'test_helper'
 
 class RailsSlugsTest < ActiveSupport::TestCase
   
-  setup :create_records
-
   test 'truth' do
     assert_kind_of Module, RailsSlugs
-  end
-
-  test 'shoud create slug' do
-    assert_equal @simple.slug, 'name'
-    assert_equal @translatable.slug, 'translatable-name'
-  end
-
-  test 'should edit slug' do
-    @simple.update_attributes :name => 'new name'
-    assert_equal @simple.slug, 'new-name'
-    @translatable.update_attributes :name => 'new translatable name'
-    assert_equal @translatable.slug, 'new-translatable-name'
-  end
-
-  test 'should not alter direct assigned slug' do
-    @simple.update_attributes :slug => 'direct slug'
-    assert_equal @simple.slug, 'direct slug'
-    @translatable.update_attributes :slug => 'translatable direct slug'
-    assert_equal @translatable.slug, 'translatable direct slug'
-  end
-
-  protected
-
-  def create_records
-    @simple = Simple.create(:name => 'name', :age => 14)
-    @translatable = Translatable.create(:name => 'translatable name', :age => 20)
   end
 
 end
