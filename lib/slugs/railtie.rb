05396f5d0b7c3745d31e29629b7aca7183f26463
Mathías Montossi <mmontossi@buyin.io>
2016-12-13 17:34:37 -0300

    Minor improvements

--- lib/slugs/railtie.rb
+++ lib/slugs/railtie.rb
@@ -1,22 +1,27 @@
 module Slugs
   class Railtie < Rails::Railtie
 
-    initializer 'slugs.extensions' do
+    initializer 'slugs.action_dispatch' do
       ::ActionDispatch::Routing::RouteSet::Generator.prepend(
         Slugs::Extensions::ActionDispatch::Generator
       )
       ::ActionDispatch::Routing::RouteSet::NamedRouteCollection::UrlHelper::OptimizedUrlHelper.prepend(
         Slugs::Extensions::ActionDispatch::OptimizedUrlHelper
       )
-      ::ActiveRecord::Base.include(
-        Slugs::Extensions::ActiveRecord::Base
-      )
-      ::ActiveRecord::Base.extend(
-        Slugs::Extensions::ActiveRecord::Finders
-      )
-      ::ActiveRecord::Relation.include(
-        Slugs::Extensions::ActiveRecord::Finders
-      )
+    end
+
+    initializer 'slugs.active_record' do
+      ActiveSupport.on_load :active_record do
+        ::ActiveRecord::Base.include(
+          Slugs::Extensions::ActiveRecord::Base
+        )
+        ::ActiveRecord::Base.extend(
+          Slugs::Extensions::ActiveRecord::Finders
+        )
+        ::ActiveRecord::Relation.include(
+          Slugs::Extensions::ActiveRecord::Finders
+        )
+      end
     end
 
     rake_tasks do
--- lib/slugs/version.rb
+++ lib/slugs/version.rb
@@ -1,5 +1,5 @@
 module Slugs
 
-  VERSION = '4.0.0.2'
+  VERSION = '4.0.0.3'
 
 end
